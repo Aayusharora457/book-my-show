c46944f606a93716795b8821707d26eb9d12c8d3
Aayusharora457 <aayusharora457@gmail.com>
2021-07-24 14:21:07 -0700

    add everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
@@ -13,6 +14,11 @@ import Plays from "./pages/Plays.page";
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// axios default settings
+axios.defaults.baseURL="https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
